f3af29e9520a7423f3b49b445679b91f7b83b225
joerlop <jj.erlich155@gmail.com>
2019-04-01 15:20:11 -0500

    Completed function conversion challenges and stretch

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -1,24 +1,35 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
-// let myFunction = function () {
-// console.log("Function was invoked!");
+//let myFunction = function () {
+  //  console.log("Function was invoked!");
 // };
-// myFunction();
+
+let myFunction = () => console.log("Function was invoked!");
+
+myFunction();
+
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
-// anotherFunction("Example");
+
+let anotherFunction = param => param;
+console.log(anotherFunction("Example"));
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = (param1, param2) => param1 + param2;
+console.log(anotherFunction(add(1,2)));
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+let subtract = (param1, param2) => param1 - param2;
+console.log(anotherFunction(subtract(1,2)));
 
 
 // Stretch
@@ -28,3 +39,9 @@
 //   return num * 3;
 // });
 // console.log(triple);
+
+exampleArray = [1,2,3,4];
+
+const triple = exampleArray.map(num => num * 3)
+
+console.log(triple);
